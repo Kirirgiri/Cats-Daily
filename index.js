46c67f36e550cbf6a578521b89ff409fbf4e7dc0
Kirirgiri <95420741+Kirirgiri@users.noreply.github.com>
2024-07-24 21:05:49 +0200

    Make some jokes with possible filtering

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import express from "express";
+import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
+  try{
+    const response = await axios.get("https://v2.jokeapi.dev");
+    const result = response.data;
+    res.render("index.ejs", {data: result});
+  }catch(error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  try{
+    const type = req.body.type;
+    const flags = req.body.flags;
+    const containsCat = req.body.contains;
+    const response = await axios.get(`https://v2.jokeapi.dev/joke/${type}?blacklistFlags=${flags ? flags : ""}&contains=${containsCat ? "cat" : ""}`);
+    const result = response.data;
+    res.render("index.ejs",{
+      data: result,
+    });
+  }catch(error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs",{
+      error: "No jokes that match your criteria.",
+    });
+  }
 });
 
 app.listen(port, () => {
